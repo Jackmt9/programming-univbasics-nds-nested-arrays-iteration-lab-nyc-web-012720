1246a1963f4cce1a0344b70c9ac732305714be2f
Jack T <jackmt9@gmail.com>
2020-01-16 02:33:34 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,3 +1,4 @@
+# require "pry"
 def join_ingredients(src)
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
@@ -6,6 +7,12 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  new_array = []
+  src.each do |array_i|
+    new_array << "I love #{array_i[0]} and #{array_i[1]} on my pizza."
+    # binding.pry
+  end
+  puts return new_array
 end
 
 def find_greater_pair(src)
